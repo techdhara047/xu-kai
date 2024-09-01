1caab8dddfdabeae12df1435afe1ffc02828c56b
techdhara047 <techdhara047@gmail.com>
2024-09-01 07:41:38 +0530

    solve card issue

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Status } from "../../utils/constants";
 import { slugify } from "../../utils/utils";
 
 function Card({ item }) {
   const images = item.data.cardimages;
-  const randomIndex = Math.floor(Math.random() * images.length);
+
   const href = slugify(item.data.title);
 
   return (
     <a href={`/filmography/${href}`}>
-      <div className="card" key={item.id + 1}>
+      <div className="card">
         {(item.data.status === 2 || item.data.status === 3) && (
           <span className="status">{Status[item.data.status]}</span>
         )}
-        <span className="episodes">{item.data.episodes}</span>
+        <p className="episodes">{item.data.episodes}</p>
 
         <figure>
-          <img src={images[randomIndex]} alt="card image" />
+          <img src={images[0]} alt="card image" />
         </figure>
         <div
           className="info"
--- src/components/Card/CardContainer.jsx
+++ src/components/Card/CardContainer.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Card from "./Card";
+import Card from "../Card.astro";
 
 function CardContainer({ data, heading }) {
   return (
