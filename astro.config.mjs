544a6a6f0ac62d7c894a5bfe843c67ae56459a1d
techdhara047 <techdhara047@gmail.com>
2024-09-05 00:07:29 +0530

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()],
+  integrations: [icon()],
 });
