b24e85c5ee089dff947bf79755411d8ecb5106e3
techdhara047 <techdhara047@gmail.com>
2024-09-06 03:40:46 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://xu-kai.netlify.app/",
+  integrations: [icon(), sitemap()],
 });
